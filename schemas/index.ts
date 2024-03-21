2cde906142f9aa75af0cbc0daa3ec635368bc8c7
Vatsal Dave <vatsaldave2002@gmail.com>
2024-03-21 12:27:53 +0530

    (feat): add register form with validations, add prisma setup

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,13 @@ export const LoginSchema = z.object({
 		.email({ message: "Invalid email address" }),
 	password: z.string().min(1, { message: "Password is required" }),
 });
+
+export const RegisterSchema = z.object({
+	email: z
+		.string({ required_error: "Email is required" })
+		.email({ message: "Invalid email address" }),
+	password: z.string().min(6, { message: "Minimum six characters required" }),
+	name: z.string().min(2, {
+		message: "Name is required",
+	}),
+});
