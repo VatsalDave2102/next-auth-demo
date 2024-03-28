5e786ef45b5326d9414392a4b5db9b6d09b7f718
Vatsal Dave <vatsaldave2002@gmail.com>
2024-03-28 18:08:46 +0530

    (feat): add settings page form

---
+++ data/account.ts
@@ -0,0 +1,10 @@
+import { db } from "@/lib/db";
+
+export const getAccountByUserId = async (userId: string) => {
+	try {
+		const account = db.account.findFirst({ where: { id: userId } });
+		return account;
+	} catch (error) {
+		return null;
+	}
+};
