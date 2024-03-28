691c16b8dfb03763c1ce191f16d8de615798d331
Vatsal Dave <vatsaldave2002@gmail.com>
2024-03-28 17:04:27 +0530

    (feat): add role based access for admin and users

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { UserInfo } from "@/components/auth/user-info";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+
+export default function ClientPage() {
+	const user = useCurrentUser();
+	return <UserInfo user={user} label="Client Component" />;
+}
